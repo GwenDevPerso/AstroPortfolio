d58bc1c6005c2ca000ece3124e1b84c1fbcb3fb2
gwenael bihan <gwenael.bihan@protonmail.com>
2025-06-17 22:09:05 +0200

    Fix GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://GwenDevPerso.github.io',
+  base: '/AstroPortfolio',
+  output: 'static',
+  build: {
+    assets: 'assets'
+  }
+});
